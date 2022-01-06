a6d0c836a93c4d8191adc5bf1ae26fffbabc6f36
ygrillo <ygrillo@tutanota.com>
2022-01-06 13:57:57 -0300

    atualização na função dec_to_bin

--- Projetos/Bin_Dec_Converter.py
+++ Projetos/Bin_Dec_Converter.py
@@ -25,4 +25,4 @@ def dec_to_bin(dec: int):
     else:
         prefix = 10
 
-    print(str(prefix) + ''.join([str(x) for x in lista[::-1]]))
+    return str(prefix) + ''.join([str(x) for x in lista[::-1]])
